ade6e870d6c03d79442127d0a8442da22ba509b4
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2014-06-10 14:07:29 +0200

    add password recovery link

--- templates/plugins/function.login_form.php
+++ templates/plugins/function.login_form.php
@@ -9,11 +9,13 @@ function smarty_function_login_form($params, $smarty) {
     <h5 class="form-signin-heading">Log in or <a href="<?php echo site_url('/account/register'); ?>">register</a>.</h5>
     <input class="form-control" type="email" placeholder="E-Mail" name="email" required autofocus/>
     <input class="form-control" type="password" placeholder="Password" name="password" required/>
-    <div class="form-group">
-        <input type="checkbox" name="remember" id="remember" value="yes"/> 
-        <label class="checkbox" for="remember">Remember me</label>
+    <div class="checkbox">
+        <label>
+            <input class="form-control" type="checkbox" name="remember" id="remember" value="yes"/> Remember me
+        </label>
     </div>
     <button class="btn btn-outline btn-block" type="submit">Login!</button>
+    <a href="<?php echo site_url('/account/recover'); ?>">Forgot your password?</a>
     <?php
     echo form_close();
 }
