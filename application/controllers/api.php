2d61a08b6af99ea2197210f728df593fcd2fe8eb
Steffen Ohrendorf <steffen.ohrendorf@gmx.de>
2015-01-27 11:36:27 +0000

    remove wrong comment and debug outputs

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -4,29 +4,6 @@ if(!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
-/**
- * User account controller.
- * 
- * The workflow for a new user is as follows:
- *
- *   1. Enter the 'register' method.  This will first display the 'account_register'
- *      template for entering the needed information.  If the information is
- *      incomplete or faulty, the 'account_register_invalid' will be shown.  Else,
- *      a preliminary account with the role {@see \addventure\User::AwaitApproval} will be
- *      created an E-mail will be sent to the user which contains encrypted data
- *      for validation.
- * 
- *   2. The user receives his E-mail with the activation link, which points to
- *      the 'verify' method.  Here, the security token passed in the URL will
- *      be verified against the stored information, and if everything is OK,
- *      the account role will be set to {@see \addventure\User::Registered}.  But if
- *      something goes wrong, the 'account_register_invalid' will be shown.
- * 
- *   3. Now, the user has to login to create his session cookie and to store
- *      the session data in the database.  This could be done in the 'verify'
- *      step, but it ensures that the E-mail account isn't hijacked, because
- *      the user has to enter his password again.
- */
 class Api extends CI_Controller
 {
 
@@ -82,56 +59,46 @@ class Api extends CI_Controller
 
     public function addcomment($docId)
     {
-        $this->output->set_content_type('text/plain');
         $this->load->library('userinfo');
         if(!$this->userinfo->user || !$this->userinfo->user->canCreateComment()) {
-echo "{error:1}";
             return;
         }
 
         $this->load->helper('xss_clean');
         $commentText = $this->input->post('comment');
         if($commentText === false) {
-echo "{error:2}";
             return;
         }
         $commentText = trim(xss_clean2(strip_tags($commentText)));
         if(empty($commentText)) {
-echo "{error:3}";
             return;
         }
         
         $authorName = $this->input->post('author');
         if($authorName === false) {
-echo "{error:4}";
             return;
         }
         $authorName = trim(strip_tags($authorName));
         if(empty($authorName)) {
-echo "{error:5}";
             return;
         }
 
         $docId = filter_var($docId, FILTER_SANITIZE_NUMBER_INT);
         if($docId === false || $docId === null) {
-echo "{error:6}";
             return;
         }
 
         $this->load->library('em');
         $episode = $this->em->findEpisode($docId);
         if(!$episode) {
-echo "{error:7}";
             return;
         }
         if($episode->getText() === NULL) {
-echo "{error:8}";
             return;
         }
         
         $author = $this->em->findOrCreateAuthorForUser($this->userinfo->user, $authorName, true);
         if(!$author) {
-echo "{error:9}";
             return;
         }
         
